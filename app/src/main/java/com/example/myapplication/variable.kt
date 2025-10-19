05df3cc433689aeecaabdd0d83326e57968a8103
Tqolh <tarik.project22@gmail.com>
2025-10-19 19:59:49 +0100

    hena dert les founction

--- app/src/main/java/com/example/myapplication/variable.kt
+++ app/src/main/java/com/example/myapplication/variable.kt
@@ -1,5 +1,32 @@
 package com.example.myapplication
 
+fun helloUser(name : String) : String{
+    return name
+
+}
+fun sommeDeuxNombre(a : Int , b : Int) : Int{
+    return  a+b
+}
+fun maxOfThree(n1: Int , n2 : Int , n3 : Int) : Int {
+    var max1 :Int = 0
+    if (n1 > n2 && n1 > n3){
+        max1 = n1
+    }else if (n2 > n1 && n2 > n3){
+        max1 = n2
+    }else{
+        max1 = n3
+    }
+    return  max1
+}
+fun isPositive(numbrr : Int): Boolean{
+    if(numbrr > 0){
+        return true
+    }else{
+        return false
+    }
+
+}
+
 fun main(){
 // ---------------------------
 // 1. Variables de base
@@ -117,7 +144,7 @@ println("exercice 5 ")
     var nom : Int = readLine()!!.toInt()
     var somme : Int =0
     for (i in 1.. nom step 2) {
-        somme =+ i
+        somme = somme + i
     }
     println("la somme c'est : $somme")
 println("exercice")
@@ -129,6 +156,12 @@ println("exercice")
     }
     println("la fac c'est : $fac")
 
+ // les founction :
+println("exercice 1  function : ")
+    println("Bounjour +"+helloUser("tarik")+", bienvenue dans kotlin")
+    println("la somme c'est : "+sommeDeuxNombre(5 , 10))
+    println("le max c'est :"+maxOfThree(5,7,2))
+    println("le nombre c'est "+isPositive(0))
 
 
 
