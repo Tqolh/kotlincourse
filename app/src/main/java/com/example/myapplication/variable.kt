98bfbaf59fa07e58a48cd964913fce04fc6399b0
Tqolh <tarik.project22@gmail.com>
2025-10-19 22:51:49 +0100

    hena zedt exeption when

--- app/src/main/java/com/example/myapplication/variable.kt
+++ app/src/main/java/com/example/myapplication/variable.kt
@@ -26,6 +26,15 @@ fun isPositive(numbrr : Int): Boolean{
     }
 
 }
+fun NoteToMention(note : Int){
+    return when (note){
+        in 0..9 -> println("Echec")
+        in 10..13 -> println("Passable")
+        in 14..16 -> println("Bien")
+        in 17..20 -> println("Excelent")
+        else -> print("error")
+    }
+}
 
 fun main(){
 // ---------------------------
@@ -162,6 +171,23 @@ println("exercice 1  function : ")
     println("la somme c'est : "+sommeDeuxNombre(5 , 10))
     println("le max c'est :"+maxOfThree(5,7,2))
     println("le nombre c'est "+isPositive(0))
+// null safety
+    println("entre un nommbre entre 1 et 7 : ")
+    var jour : Int =readLine()!!.toInt()
+    when(jour){
+        1 -> println("lundi")
+        2 -> println("Mardi")
+        3 -> println("Mercredi")
+        4 -> println("Jeudi")
+        5 -> println("Vendredi")
+        6 -> println("Samedi")
+        7 -> println("Dimanche")
+        else -> println("en ne trouve pas se jour")
+    }
+    println("votre niveau est :"+NoteToMention(10))
+
+
+
 
 
 
