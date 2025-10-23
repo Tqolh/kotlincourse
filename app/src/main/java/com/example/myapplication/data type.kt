6fee187c23a97dd11ceee75c368f3e2c5e5c90f9
Tqolh <tarik.project22@gmail.com>
2025-10-23 19:25:13 +0100

    hade fichier 5ase be data type ou var ou val

---
+++ app/src/main/java/com/example/myapplication/data type.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+fun main(){
+    val Name : String = "Tarik Oulehiane" // val = mkn9drouche nbdelo 9ima ta3ha
+    var Age : Int = 20 // var = n9dero nbdelo 9ima ta3ha
+    println("You username is : $Name")
+    println("your age is : $Age")
+    Age = 21
+    println("you change your age, your age is : $Age")
+
+
+    val name : String = "tarik"
+    var age : Int = 21
+    var annoyance : Double = 15.15
+    var letter : Char = '$'
+    var student : Boolean = true
+
+    println("\n My name is : $name , I'm : $age , I get last years  : $annoyance , my favorite character is : $letter , I'm Student : $student")
+}
--- app/src/main/java/com/example/myapplication/variable.kt
+++ app/src/main/java/com/example/myapplication/variable.kt
@@ -37,31 +37,6 @@ fun NoteToMention(note : Int){
 }
 
 fun main(){
-// ---------------------------
-// 1. Variables de base
-// ---------------------------
-    val Name : String = "Tarik Oulehiane" // val = mkn9drouche nbdelo 9ima ta3ha
-    var Age : Int = 20 // var = n9dero nbdelo 9ima ta3ha
-    println("You username is : $Name")
-    println("your age is : $Age")
-    Age = 21
-    println("you change your age, your age is : $Age")
-
-
-
-
-
-// -------------------------------------------
-// 3. Data Types :
-// " Boolean , Int , Double , String , char "
-// -------------------------------------------
-    val name : String = "tarik"
-    var age : Int = 21
-    var annoyance : Double = 15.15
-    var letter : Char = '$'
-    var student : Boolean = true
-
-    println("\n My name is : $name , I'm : $age , I get last years  : $annoyance , my favorite character is : $letter , I'm Student : $student")
 // Input  and condition in Kotlin
 //exercice : 1
     println("exercice 1")
