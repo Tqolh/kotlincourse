83216711752838d6fd584cb268dba89dcf77ec71
Tqolh <tarik.project22@gmail.com>
2025-10-23 19:42:52 +0100

    hade fichier 5as be When

--- app/src/main/java/com/example/myapplication/variable.kt
+++ app/src/main/java/com/example/myapplication/variable.kt
@@ -1,31 +1,11 @@
 package com.example.myapplication
 
 
-fun NoteToMention(note : Int){
-    return when (note){
-        in 0..9 -> println("Echec")
-        in 10..13 -> println("Passable")
-        in 14..16 -> println("Bien")
-        in 17..20 -> println("Excelent")
-        else -> print("error")
-    }
-}
+
 fun main(){
 
 // when
-    println("entre un nommbre entre 1 et 7 : ")
-    var jour : Int =readLine()!!.toInt()
-    when(jour){
-        1 -> println("lundi")
-        2 -> println("Mardi")
-        3 -> println("Mercredi")
-        4 -> println("Jeudi")
-        5 -> println("Vendredi")
-        6 -> println("Samedi")
-        7 -> println("Dimanche")
-        else -> println("en ne trouve pas se jour")
-    }
-    println("votre niveau est :"+NoteToMention(10))
+
 
 
 
