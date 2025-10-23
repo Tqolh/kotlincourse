492dd6858b92080ce6e880277731d5543915c0c4
Tqolh <tarik.project22@gmail.com>
2025-10-23 19:33:19 +0100

    hade fichier 5as be les boucle

--- app/src/main/java/com/example/myapplication/variable.kt
+++ app/src/main/java/com/example/myapplication/variable.kt
@@ -37,58 +37,7 @@ fun NoteToMention(note : Int){
 }
 
 fun main(){
-// Les Boucles : " For , While , Do While"
-// exercice 1 :
-    println("exercice 1")
-
-    for ( i in 1 .. 10){
-        println("i = $i")
-    }
-// exercice 2 :
-    println("exercice 2 ")
-    for ( i in 1 .. 20){
-        if(i % 2 == 0){
-            println("i = $i")
-        }
-    }
-// exxercice 3 :
-
-    println("exerccice 3")
-    println("entre le nombre pour table de multiple : ")
-    var nombre : Int = readLine()!!.toInt()
-    for (i in 1..10){
-        println("$nombre * $i = ${nombre * i }")
-    }
-// exercice 4 :
-println("exercice 4")
-    println("entre un nombre : ")
-    var c : Int = readLine()!!.toInt()
-    var s :Int = 0
-    for (i in c downTo 1) {
-        println(i)
-        s=s+i
-    }
-    println("la somme c'est : $s")
-
-// exercice 5 :
-println("exercice 5 ")
-    println("entre un nombre")
-    var nom : Int = readLine()!!.toInt()
-    var somme : Int =0
-    for (i in 1.. nom step 2) {
-        somme = somme + i
-    }
-    println("la somme c'est : $somme")
-println("exercice")
-    println("entre un nombre ::::::")
-    var va : Int = readLine()!!.toInt()
-    var fac : Int =1
-    for (i in 1 .. va ) {
-        fac *= i
-    }
-    println("la fac c'est : $fac")
-
- // les founction :
+    // les founction :
 println("exercice 1  function : ")
     println("Bounjour +"+helloUser("tarik")+", bienvenue dans kotlin")
     println("la somme c'est : "+sommeDeuxNombre(5 , 10))
